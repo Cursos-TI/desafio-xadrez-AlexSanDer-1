beba0d311ac28ff36829e6c15e6938c6ff416729
AlexSanDer-1 <odhsanderspindola@gmail.com>
2025-04-20 22:11:32 +0000

    Aplicando Recursividade para movimentação  de peças

--- xadrez.c
+++ xadrez.c
@@ -4,6 +4,39 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
+
+
+ /*Funções recursivas pode ser usadas para substituir loops,
+nesse caso para movimentação das peças xadrez */
+
+//Funções recursivas
+void moverTorre(int casas)
+{
+    if (casas > 0)
+    {
+        printf("Direta\n");
+        moverTorre(casas - 1);
+    }
+}                                   
+
+void moverRainha(int casas)
+{
+    if (casas > 0)
+    {
+        printf("Esquerda\n");
+        moverRainha(casas - 1);
+    }
+}
+
+void moverBispo(int casas)
+{
+    if (casas > 0)
+    {
+        printf("Cima,Direita\n");
+        moverBispo(casas - 1);
+    }
+}
+
 int main()
 {
 
@@ -38,16 +71,20 @@ int main()
 
     printf("*** MOVIMENTO CAVALO ***\n");
 
-    // Movimento  em L Cavalo 
-    for (int i = 0; i < 1; i++) //Loops alinhados
+    // Movimento  em L Cavalo
+    for (int i = 0; i < 1; i++) // Loops alinhados
     {
-        printf("Direita\n");   //Imprime direita uma vez    
+        printf("Direita\n"); // Imprime direita uma vez
         for (int k = 0; k < 2; k++)
         {
-            printf("Cima\n");  //Imprime cima duas vezes
+            printf("Cima\n"); // Imprime cima duas vezes
         }
     }
 
+    printf("*** RECURSIVIDADE  ***\n");
+    moverRainha(8);
+    moverTorre(5);
+    moverBispo(5);
+    
     return 0;
-
 }
