cbf49ed8ce433b6fc0670369104a182bab7fac25
AlexSanDer-1 <odhsanderspindola@gmail.com>
2025-04-20 21:02:57 +0000

    Movimento Cavalo em L

--- xadrez.c
+++ xadrez.c
@@ -4,7 +4,8 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
-int main(){
+int main()
+{
 
     int r = 1; // Rainha
     int b = 1; // Bispo
@@ -35,5 +36,18 @@ int main(){
         b++;
     } while (b <= 5);
 
+    printf("*** MOVIMENTO CAVALO ***\n");
+
+    // Movimento  em L Cavalo 
+    for (int i = 0; i < 1; i++) //Loops alinhados
+    {
+        printf("Direita\n");   //Imprime direita uma vez    
+        for (int k = 0; k < 2; k++)
+        {
+            printf("Cima\n");  //Imprime cima duas vezes
+        }
+    }
+
     return 0;
+
 }
